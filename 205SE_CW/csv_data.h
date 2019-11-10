06395bf37e5bf2033cb2c121fd061a604f5295a2
Amitesh Nagarkar <nagarka@uni.coventry.ac.uk>
2019-11-10 18:29:00 +0000

    data is now into a vector data structure & updated readme with test data

---
+++ 205SE_CW/csv_data.h
@@ -0,0 +1,19 @@
+#include <string>
+
+using namespace std;
+
+class csv_data
+{
+	public:
+	string date;
+	string open;
+	string high;
+	string low;
+	string close;
+	string volume;
+	string marketCap;
+
+
+
+};
+
